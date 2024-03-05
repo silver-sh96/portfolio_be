ba169b639a3388ff4f5fb44b752dad824b90ab13
silver-sh96 <cubha@naver.com>
2024-03-05 21:15:48 +0900

    Initial commit_2024.03.05

---
+++ src/main/java/com/esh/portfolio_be/PortfolioBeApplication.java
@@ -0,0 +1,13 @@
+package com.esh.portfolio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBeApplication.class, args);
+	}
+
+}
